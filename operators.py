31df4dbd700edb53bf2e26175f4e6133f85ed13f
Dharane34 <dharane34@gmail.com>
2025-01-03 11:07:39 +0530

    python basics
    I have learned about the operators and the if else logic

---
+++ operators.py
@@ -0,0 +1,57 @@
+# code 1 
+a = 5
+b = 10
+# additon
+print("addition", a+b)
+# subraction
+print("subraction", a-b)
+# division
+print("division", a/b)
+# multiplication
+print("multiplication", a*b)
+# a greator than b
+print("greater than",a>b)
+# a less than b
+print("less than",a<b)
+# a and b are equal
+print("equal to",a==b)
+
+# code 2
+# addition
+add=a+b
+#  subration 
+sub= a-b
+# division 
+div= a/b
+# multiplication
+mul=a*b
+# greater than
+gre= a>b
+# less than
+less= a<b
+# equal to
+equal= a==b
+print(a+b,a-b,a/b,a*b,a>b,a<b,a==b)
+
+# code3
+# additon
+sum=a+b
+print("The additon of two numbers",sum)
+# subraction
+sum2=a-b
+print("The subraction of the two numbers",sum2)
+# divition
+sum3=a/b
+print("The division of the two numbers",sum3)
+# multiplication
+sum4=a*b
+print("The multiplication of the two numbers",sum4)
+# greater than
+sum5=a>b
+print("a greater than b",sum5)
+# lesser than
+sum6=a<b
+print("a lesser than b",sum6)
+# equal to
+sum7=a==b
+print("a equal to b",sum7)
